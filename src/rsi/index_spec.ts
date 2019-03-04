b95f20f8b0af5e344bf918e65f8ab19a4586b5b1
Matt Raible <matt.raible@okta.com>
2019-03-03 20:37:31 -0700

    Initial import

---
+++ src/rsi/index_spec.ts
@@ -0,0 +1,22 @@
+import { HostTree } from '@angular-devkit/schematics';
+import { SchematicTestRunner, UnitTestTree } from '@angular-devkit/schematics/testing';
+import * as path from 'path';
+import packageJson from './react-pkg.json';
+
+const collectionPath = path.join(__dirname, '../collection.json');
+
+describe('rsi', () => {
+  it('works', () => {
+    const tree = new UnitTestTree(new HostTree);
+    tree.create('/package.json', JSON.stringify(packageJson));
+
+    const runner = new SchematicTestRunner('schematics', collectionPath);
+    runner.runSchematic('rsi', {}, tree);
+
+    expect(tree.files.length).toEqual(3);
+    expect(tree.files.sort()).toEqual(['/package.json', '/src/App.js', '/src/index.js']);
+
+    const mainContent = tree.readContent('/src/index.js');
+    expect(mainContent).toContain(`import 'bootstrap/dist/css/bootstrap.min.css'`);
+  });
+});
