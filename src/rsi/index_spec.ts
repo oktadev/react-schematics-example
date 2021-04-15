1e7657f1cdc0d5307428399b02caf51090ad285c
Matt Raible <matt.raible@okta.com>
2021-04-15 13:41:33 -0600

    Update to use React 17 and Schematics 11

--- src/rsi/index_spec.ts
+++ src/rsi/index_spec.ts
@@ -6,17 +6,18 @@ import packageJson from './react-pkg.json';
 const collectionPath = path.join(__dirname, '../collection.json');
 
 describe('rsi', () => {
-  it('works', () => {
+  it('works', (done) => {
     const tree = new UnitTestTree(new HostTree);
     tree.create('/package.json', JSON.stringify(packageJson));
 
     const runner = new SchematicTestRunner('schematics', collectionPath);
-    runner.runSchematic('rsi', {}, tree);
+    runner.runSchematicAsync('rsi', {}, tree).toPromise().then(tree => {
+      expect(tree.files.length).toEqual(3);
+      expect(tree.files.sort()).toEqual(['/package.json', '/src/App.js', '/src/index.js']);
 
-    expect(tree.files.length).toEqual(3);
-    expect(tree.files.sort()).toEqual(['/package.json', '/src/App.js', '/src/index.js']);
-
-    const mainContent = tree.readContent('/src/index.js');
-    expect(mainContent).toContain(`import 'bootstrap/dist/css/bootstrap.min.css'`);
+      const mainContent = tree.readContent('/src/index.js');
+      expect(mainContent).toContain(`import 'bootstrap/dist/css/bootstrap.min.css'`);
+      done();
+    }, done.fail);
   });
 });
